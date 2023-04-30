79c7e37ab4531a0ff340ee2fe83d717d16522738
Weshtey74 <weshtey@gmail.com>
2023-04-30 11:13:43 -0700

    Fourth task commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *lowkey;
 	listint_t *temp = *head;
 
-	new = malloc(sizeof(listint_t));
+	lowkey = malloc(sizeof(listint_t));
 	if (!lowkey)
 		return (NULL);
 
