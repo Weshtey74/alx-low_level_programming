0f6115645005eb5aab0213e9d6397044432b6e5f
Weshtey74 <weshtey@gmail.com>
2023-04-30 10:39:44 -0700

    Second Task commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * listint_len - this function that the number of elements in a linked listint_t list
- * @h: the linked list 'listint_t' to traverse
- *
- * Return: number of nodes
- */
+*listint_len - this function returns number of elements linked in listint_t
+* @h: the linked list 'listint_t' to traverse
+*
+* Return: number of nodes
+*/
 size_t listint_len(const listint_t *h)
 {
 	size_t numb = 0;
